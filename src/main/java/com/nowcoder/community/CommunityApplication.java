e18336a1bff8ec052f582d58e1eae6becb4f1b27
jellmo <942560621@qq.com>
2022-10-08 22:00:59 +0800

    Improve the database configuration and entity class

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -1,5 +1,6 @@
 package com.nowcoder.community;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author jellmo
  */
 @SpringBootApplication
+@MapperScan("com.nowcoder.community.dao")
 public class CommunityApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/nowcoder/community/model/entity/DiscussPostEntity.java
@@ -0,0 +1,109 @@
+package com.nowcoder.community.model.entity;
+
+import java.util.Date;
+
+/**
+ * @author: jellmo
+ * @date: 2022/10/8 21:55
+ */
+public class DiscussPostEntity {
+
+    private Integer id;
+    private Integer userId;
+    private String title;
+    private String content;
+    /**贴文类型：0-普通; 1-置顶;**/
+    private Integer type;
+    /**贴文状态：0-正常; 1-精华; 2-拉黑;**/
+    private Integer status;
+    private Date createTime;
+    private Integer commentCount;
+    private Double score;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(Integer commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "DiscussPostEntity{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", type=" + type +
+                ", status=" + status +
+                ", createTime=" + createTime +
+                ", commentCount=" + commentCount +
+                ", score=" + score +
+                '}';
+    }
+}
